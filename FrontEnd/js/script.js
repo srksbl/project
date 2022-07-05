c374fe5f8c85b1f07e984a26a6ed6df71b2ba94f
srksbl <shahrukhahmad142@gmail.com>
2022-07-05 22:10:15 +0530

    added sweet alert and implemented delete funcaitonality

--- FrontEnd/js/script.js
+++ FrontEnd/js/script.js
@@ -1,80 +1,122 @@
 /**
  * Function to create new record
- * @param {*} event 
+ * @param {*} event
  */
 const createNewRecord = (event) => {
-    event.preventDefault();
-    
-    const firstName = document.querySelector('#first_name').value;
-    const lastName = document.querySelector('#last_name').value;
-    const mobile = document.querySelector('#mobile').value;
+  event.preventDefault();
 
-    const isValid = validateForm(firstName, lastName, mobile);
-    if(isValid) {
-        // create record by axios
-        const data = {
-            "first_name": firstName,
-            "last_name": lastName,
-            "mobile": mobile
-        }
-        createRecordByAPI(data);
-    }
+  const firstName = document.querySelector("#first_name").value;
+  const lastName = document.querySelector("#last_name").value;
+  const mobile = document.querySelector("#mobile").value;
 
-}
+  const isValid = validateForm(firstName, lastName, mobile);
+  if (isValid) {
+    // create record by axios
+    const data = {
+      first_name: firstName,
+      last_name: lastName,
+      mobile: mobile,
+    };
+    createRecordByAPI(data);
+  }
+};
 
 /**
  * Function to validate create record form
- * @param {string} firstName 
- * @param {string} lastName 
- * @param {string} mobile 
+ * @param {string} firstName
+ * @param {string} lastName
+ * @param {string} mobile
  * @returns boolean
  */
 const validateForm = (firstName, lastName, mobile) => {
-    // write validation code here
-    return true;
-}
+  removedAlreadyPlacesErrorState();
+  // write validation code here
+  var isValid = true;
+
+  if (firstName == "") {
+    isValid = false;
+    document.querySelector("#first_name_error").innerText =
+      "This is required field!";
+  }
+
+  if (lastName == "") {
+    isValid = false;
+    document.querySelector("#last_name_error").innerText =
+      "This is required field!";
+  }
+
+  if (mobile == "") {
+    isValid = false;
+    document.querySelector("#mobile_error").innerText =
+      "This is required field!";
+  } else if (!/^[0-9]+$/.test(mobile)) {
+    isValid = false;
+    document.querySelector("#mobile_error").innerText =
+      "Mobile number must be digits!";
+  } else if (mobile.length != 10) {
+    isValid = false;
+    document.querySelector("#mobile_error").innerText =
+      "Mobile number must be 10 digits!";
+  }
+
+  // return flag to check in calling place
+  return isValid;
+};
+
+/**
+ * Function to removed displayed error in next time
+ */
+const removedAlreadyPlacesErrorState = () => {
+  document.querySelector("#first_name_error").innerText = null;
+  document.querySelector("#last_name_error").innerText = null;
+  document.querySelector("#mobile_error").innerText = null;
+};
 
 /**
  * Function to make API post call to create new crecord
- * @param {object} data 
+ * @param {object} data
  * @returns null
  */
 const createRecordByAPI = (data) => {
-    // hit axios with post request or call API
-    axios.post("http://localhost:3000/contacts", data)
-    .then( function(response) {
-        document.getElementById("response-msg").innerHTML = `
+  // hit axios with post request or call API
+  axios
+    .post("http://localhost:3000/contacts", data)
+    .then(function (response) {
+      document.getElementById("response-msg").innerHTML = `
             <div class="alert alert-success" role="alert">
                 <i class="fa-solid fa-info-circle"></i> Record successfully created!
             </div>
         `;
-        loadAllContactsList();
+
+      loadAllContactsList();
+      clearAllInputFields();
     })
-    .catch( (error) => {
-        document.getElementById("response-msg").innerHTML = `
+    .catch((error) => {
+      document.getElementById("response-msg").innerHTML = `
             <div class="alert alert-danger" role="alert">
                 <i class="fa-solid fa-info-circle"></i> ${error.message}
             </div>
         `;
     });
 
-    // to hide alert after 5 seconds
-    setTimeout(() => {
-        document.getElementById("response-msg").innerHTML = null;
-    }, 5000);
-}
+  // to hide alert after 5 seconds
+  setTimeout(() => {
+    document.getElementById("response-msg").innerHTML = null;
+  }, 5000);
+};
 
 /**
  * Function to fetch all records
  */
 const loadAllContactsList = () => {
-    axios.get("http://localhost:3000/contacts")
-    .then( (response) => {
-        console.log(response.data);
+  axios
+    .get("http://localhost:3000/contacts")
+    .then((response) => {
+      console.log(response.data);
 
-        let html = "";
-        response.data.forEach(element => {
-            html += `
+      let html = "";
+      response.data.forEach((element) => {
+        html += `
             <div class="card mb-2">
                 <div class="card-body">
                     <div class="row">
@@ -94,19 +136,63 @@ const loadAllContactsList = () => {
                             <button type="button" class="btn btn-info btn-sm card-link">
                                 <i class="fa-solid fa-edit"></i> Edit
                             </button>
-                            <button type="button" class="btn btn-danger btn-sm card-link">
+                            <button type="button" class="btn btn-danger btn-sm card-link" onclick='deleteRecord(${element.id})'>
                                 <i class="fa-solid fa-trash"></i> Delete
                             </button>
                         </div>
                     </div>
                 </div>
             </div>`;
-        });
+      });
 
-        document.getElementById("show-rcords-list").innerHTML = html;
-
+      document.getElementById("show-rcords-list").innerHTML = html;
     })
-    .catch( (error) => {
-        alert(error.message);
+    .catch((error) => {
+      alert(error.message);
     });
-}
+};
+
+/**
+ * Function to clear input field after new record created successfully
+ */
+const clearAllInputFields = () => {
+  document.getElementById("first_name").value = "";
+  document.getElementById("last_name").value = "";
+  document.getElementById("mobile").value = "";
+};
+
+const deleteRecord = (id) => {
+  Swal.fire({
+    title: "Do you want to delete this?",
+    showCancelButton: true,
+    confirmButtonText: "Confirm",
+    denyButtonText: `Cancel`,
+  }).then((result) => {
+    if (result.isConfirmed) {
+        callDeletApi(id);
+    }    
+  });
+};
+
+/**
+ * Function to hit delete api on server to delete record
+ * @param {string} id
+ */
+const callDeletApi = (id) => {
+  axios.delete(`http://localhost:3000/contacts/${id}`)
+    .then(function (res) {
+        Swal.fire("Deleted!", "", "success");
+        loadAllContactsList();
+    })
+    .catch(function (error) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: error.message
+        });
+    });
+
+  setTimeout(() => {
+    document.querySelector("#show-records-list-alert").innerHTML = null;
+  }, 5000);
+};
